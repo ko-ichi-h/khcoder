351323c0815eed2e63294d5d28590532b829fa7e
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2003-02-26 11:17:56 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@57 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/gui_OtherWin/win32.pm
+++ core/kh_lib/gui_OtherWin/win32.pm
@@ -7,9 +7,16 @@ sub _open{
 	my $self = shift;
 	my $t = $self->target;
 	if ( Win32::IsWinNT() ){
-		system "start cmd.exe /c start $t";
+		my $cmd;
+		if ($t =~ /^http/o){
+			$cmd = "start /MIN cmd.exe /c start $t";
+		} else {
+			$cmd = "start /MIN cmd.exe /c \"$t\"";
+		}
+#		print "$cmd\n";
+		system $cmd;
 	} else {
-		system "start command.com /c start $t";
+		system "start command.com /c start \"$t\"";
 	}
 }
 
