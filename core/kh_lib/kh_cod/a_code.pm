cbd31a8b1696794520db99a593b6ea24c8ce53c1
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2003-04-23 10:55:35 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@84 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/kh_cod/a_code.pm
+++ core/kh_lib/kh_cod/a_code.pm
@@ -1,5 +1,6 @@
 package kh_cod::a_code;
 use kh_cod::a_code::atom;
+use mysql_exec;
 use strict;
 
 sub count{
@@ -8,36 +9,27 @@ sub count{
 	unless ($self->{condition}){
 		return 0;
 	}
+	unless ($self->tables){
+		return 0;
+	}
 	
 	my $sql = "SELECT count(*)\n";
 	$sql .= "FROM $tani\n";
-	foreach my $i ($self->tables){
+	foreach my $i (@{$self->tables}){
 		$sql .= "\tLEFT JOIN $i ON $tani.id = $i.id\n";
 	}
 	$sql .= "WHERE\n";
 	foreach my $i (@{$self->{condition}}){
 		$sql .= "\t".$i->expr."\n";
 	}
-	
-	print "$sql";
+	# print "$sql";
 	
 	return mysql_exec->select($sql,1)->hundle->fetch->[0];
 }
 
 sub tables{
 	my $self = shift;
-	unless ($self->{condition}){
-		return 0;
-	}
-	my @t;
-	foreach my $i (@{$self->{condition}}){
-		my $at = $i->tables;
-		unless ($at){next;}
-		foreach my $h (@{$at}){
-			push @t, $h;
-		}
-	}
-	return @t;
+	return $self->{tables};
 }
 
 sub ready{
@@ -47,8 +39,67 @@ sub ready{
 		return 0;
 	}
 	
+	# ATOMごとのテーブルを作製
+	my ($n0, $n1,@t) = (0,0); 
 	foreach my $i (@{$self->{condition}}){
 		$i->ready($tani);
+		if ($i->tables){
+			$n0 += @{$i->tables};
+			if ($n0 > 25){
+				++$n1; $n0 = 0;
+			}
+			$i->parent_table("ct_$tani"."_$n1");
+			foreach my $h (@{$i->tables}){
+				push @{$t[$n1]}, $h;
+			}
+		}
+	}
+	
+	# ATOMテーブルをまとめる
+	my $n = 0;
+	foreach my $i (@t){
+		# テーブル作製
+		mysql_exec->drop_table("ct_$tani"."_$n");
+		my $sql =
+			"CREATE TABLE ct_$tani"."_$n ( id int primary key not null,\n";
+		foreach my $h (@{$i}){
+			my $col = (split /\_/, $h)[2].(split /\_/, $h)[3];
+			$sql .= "$col INT,"
+		}
+		chop $sql;
+		$sql .= ')';
+		mysql_exec->do($sql,1);
+		push @{$self->{tables}}, "ct_$tani"."_$n";
+		
+		# INSERT
+		$sql = '';
+		$sql .= "INSERT INTO ct_$tani"."_$n\n(id,";
+		foreach my $h (@{$i}){
+			my $col = (split /\_/, $h)[2].(split /\_/, $h)[3];
+			$sql .= "$col,";
+		}
+		chop $sql;
+		$sql .= ")\n";
+		$sql .= "SELECT $tani.id,";
+		foreach my $h (@{$i}){
+			$sql .= "$h.num,";
+		}
+		chop $sql;
+		$sql .= "\n";
+		$sql .= "FROM $tani \n";
+		foreach my $h (@{$i}){
+			$sql .= "\tLEFT JOIN $h ON $tani.id = $h.id\n"
+		}
+		$sql .= "WHERE ";
+		my $nn = 0;
+		foreach my $h (@{$i}){
+			if ($nn){ $sql .= ' OR '; }
+			$sql .= " $h.num ";
+			++$nn;
+		}
+		mysql_exec->do($sql,1);
+		
+		++$n;
 	}
 }
 
--- core/kh_lib/kh_cod/a_code/atom/word.pm
+++ core/kh_lib/kh_cod/a_code/atom/word.pm
@@ -5,7 +5,22 @@ use strict;
 use mysql_a_word;
 use mysql_exec;
 
+#-----------------#
+#   SQL文の準備   #
+#-----------------#
+
 my %sql_join = (
+	'bun' =>
+		'bun.id = hyosobun.bun_idt',
+	'dan' =>
+		'
+			    dan.dan_id = hyosobun.dan_id
+			AND dan.h5_id = hyosobun.h5_id
+			AND dan.h4_id = hyosobun.h4_id
+			AND dan.h3_id = hyosobun.h3_id
+			AND dan.h2_id = hyosobun.h2_id
+			AND dan.h1_id = hyosobun.h1_id
+		',
 	'h5' =>
 		'
 			    h5.h5_id = hyosobun.h5_id
@@ -14,12 +29,47 @@ my %sql_join = (
 			AND h5.h2_id = hyosobun.h2_id
 			AND h5.h1_id = hyosobun.h1_id
 		',
+	'h4' =>
+		'
+			    h4.h4_id = hyosobun.h4_id
+			AND h4.h3_id = hyosobun.h3_id
+			AND h4.h2_id = hyosobun.h2_id
+			AND h4.h1_id = hyosobun.h1_id
+		',
+	'h3' =>
+		'
+			    h3.h3_id = hyosobun.h3_id
+			AND h3.h2_id = hyosobun.h2_id
+			AND h3.h1_id = hyosobun.h1_id
+		',
+	'h2' =>
+		'
+			    h2.h2_id = hyosobun.h2_id
+			AND h2.h1_id = hyosobun.h1_id
+		',
+	'h1' =>
+		'h1.h1_id = hyosobun.h1_id'
 );
 my %sql_group = (
+	'bun' =>
+		'hyosobun.bun_idt',
+	'dan' =>
+		'hyosobun.dan_id, hyosobun.h5_id, hyosobun.h4_id, hyosobun.h3_id, hyosobun.h2_id, hyosobun.h1_id',
 	'h5' =>
 		'hyosobun.h5_id, hyosobun.h4_id, hyosobun.h3_id, hyosobun.h2_id, hyosobun.h1_id',
+	'h4' =>
+		'hyosobun.h4_id, hyosobun.h3_id, hyosobun.h2_id, hyosobun.h1_id',
+	'h3' =>
+		'hyosobun.h3_id, hyosobun.h2_id, hyosobun.h1_id',
+	'h2' =>
+		'hyosobun.h2_id, hyosobun.h1_id',
+	'h1' =>
+		'hyhyosobun.h1_id',
 );
 
+#--------------------#
+#   WHERE節用SQL文   #
+#--------------------#
 
 sub expr{
 	my $self = shift;
@@ -29,7 +79,8 @@ sub expr{
 	my ($sql, $n) = ('',0);
 	foreach my $i (@{$t}){
 		if ($n){$sql .= ' or '}
-		$sql .= "$i.num";
+		my $col = (split /\_/, $i)[2].(split /\_/, $i)[3];
+		$sql .= "IFNULL(".$self->parent_table.".$col,0)";
 		++$n;
 	}
 	if ($n > 1){
@@ -38,12 +89,21 @@ sub expr{
 	return $sql;
 }
 
+#---------------------------------------#
+#   コーディング準備（tmp table作成）   #
+#---------------------------------------#
+
 sub ready{
 	my $self = shift;
 	my $tani = shift;
 	
-	my $list = mysql_a_word->new(genkei => $self->raw)->genkei_ids;
-	unless ($list){
+	my $list = mysql_a_word->new(
+		genkei => $self->raw
+	)->genkei_ids;
+	unless (defined($list) ){
+		print Jcode->new(
+			"no such word in the text: \"".$self->raw."\"\n"
+		)->sjis;
 		return '';
 	}
 	
@@ -74,9 +134,24 @@ sub ready{
 	}
 }
 
+#-------------------------------#
+#   利用するtmp tableのリスト   #
+
 sub tables{
 	my $self = shift;
 	return $self->{tables};
+}
+
+#----------------#
+#   親テーブル   #
+sub parent_table{
+	my $self = shift;
+	my $new  = shift;
+	
+	if (length($new)){
+		$self->{parent_table} = $new;
+	}
+	return $self->{parent_table};
 }
 
 sub pattern{
@@ -85,5 +160,8 @@ sub pattern{
 sub name{
 	return 'word';
 }
+
+
+
 
 1;
