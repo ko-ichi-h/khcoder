1088b3327531e8db2d86f41396376e50b166b5ac
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2003-05-09 20:42:19 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@119 ab1598f5-0ef3-491f-8894-8c1f2390833e

---
+++ kh_lib/kh_cod/a_code/atom/string.pm
@@ -0,0 +1,147 @@
+# 生の文字列による指定
+
+package kh_cod::a_code::atom::string;
+use base qw(kh_cod::a_code::atom);
+use strict;
+
+use mysql_exec;
+
+#-----------------#
+#   SQL文の準備   #
+#-----------------#
+
+my %sql_join = (
+	'bun' =>
+		'bun.id = bun_r.id',
+	'dan' =>
+		'
+			    dan.dan_id = bun_r.dan_id
+			AND dan.h5_id = bun_r.h5_id
+			AND dan.h4_id = bun_r.h4_id
+			AND dan.h3_id = bun_r.h3_id
+			AND dan.h2_id = bun_r.h2_id
+			AND dan.h1_id = bun_r.h1_id
+		',
+	'h5' =>
+		'
+			    h5.h5_id = bun_r.h5_id
+			AND h5.h4_id = bun_r.h4_id
+			AND h5.h3_id = bun_r.h3_id
+			AND h5.h2_id = bun_r.h2_id
+			AND h5.h1_id = bun_r.h1_id
+		',
+	'h4' =>
+		'
+			    h4.h4_id = bun_r.h4_id
+			AND h4.h3_id = bun_r.h3_id
+			AND h4.h2_id = bun_r.h2_id
+			AND h4.h1_id = bun_r.h1_id
+		',
+	'h3' =>
+		'
+			    h3.h3_id = bun_r.h3_id
+			AND h3.h2_id = bun_r.h2_id
+			AND h3.h1_id = bun_r.h1_id
+		',
+	'h2' =>
+		'
+			    h2.h2_id = bun_r.h2_id
+			AND h2.h1_id = bun_r.h1_id
+		',
+	'h1' =>
+		'h1.h1_id = bun_r.h1_id'
+);
+
+my $num = 0;
+sub reset{
+	$num = 0;
+}
+
+#--------------------#
+#   WHERE節用SQL文   #
+#--------------------#
+
+sub expr{
+	my $self = shift;
+	my $t = $self->tables;
+	unless ($t){ return '0';}
+	
+	$t = $t->[0];
+	my $col = (split /\_/, $t)[2].(split /\_/, $t)[3];
+	my $sql = "IFNULL(".$self->parent_table.".$col,0)";
+	return $sql;
+}
+
+#---------------------------------------#
+#   コーディング準備（tmp table作成）   #
+#---------------------------------------#
+
+sub ready{
+	my $self = shift;
+	my $tani = shift;
+
+	# テーブル名
+	my $table = "ct_$tani"."_string_$num";
+	$self->{tables} = ["$table"];
+	++$num;
+	
+	# テーブル作製
+	mysql_exec->drop_table($table);
+	mysql_exec->do("
+		CREATE TABLE $table (
+			id INT primary key not null,
+			num INT
+		)
+	",1);
+
+	# クエリの取得
+	my $query = $self->raw;
+	chop $query;
+	substr($query,0,1) = '';
+
+	# INSERT
+	my $sql;
+	$sql = "
+		INSERT
+		INTO $table (id, num)
+		SELECT $tani.id, count(*)
+		FROM $tani, bun_r
+		WHERE
+			$sql_join{$tani} AND
+			rowtxt like \'%$query%\'
+		GROUP BY $tani.id
+	";
+	mysql_exec->do($sql,1);
+
+
+
+}
+
+#-------------------------------#
+#   利用するtmp tableのリスト   #
+
+sub tables{
+	my $self = shift;
+	return $self->{tables};
+}
+
+#----------------#
+#   親テーブル   #
+sub parent_table{
+	my $self = shift;
+	my $new  = shift;
+	
+	if (length($new)){
+		$self->{parent_table} = $new;
+	}
+	return $self->{parent_table};
+}
+
+sub pattern{
+	return '^\'.+\'$';
+}
+sub name{
+	return 'string';
+}
+
+1;
