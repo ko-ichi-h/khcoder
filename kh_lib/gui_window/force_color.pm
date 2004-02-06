91246d9459808134b24f4f53cb9079b9ff8ad2fb
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2004-02-06 05:52:04 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@190 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_lib/gui_window/force_color.pm
+++ kh_lib/gui_window/force_color.pm
@@ -12,7 +12,7 @@ sub _new{
 	my $win = $mw->Toplevel;
 	$win->title(Jcode->new('強調する言葉')->sjis);
 
-	my $lf = $win->Frame(
+	my $lf = $win->LabFrame(
 		-label => 'List',
 		-labelside => 'acrosstop',
 		-borderwidth => 2,
@@ -45,8 +45,6 @@ sub _new{
 	$plis->header('create',0,-text => Jcode->new('抽出語 / 文字列')->sjis);
 	$plis->header('create',1,-text => Jcode->new('タイプ')->sjis);
 
-
-
 	$win->Button(
 		-text => Jcode->new('キャンセル')->sjis,
 		-font => "TKFN",
@@ -61,8 +59,38 @@ sub _new{
 		-command => sub{ $mw->after(10,sub{$self->_make_new;});}
 	)->pack(-side => 'right');
 	
+	$self->{list}    = $plis;
 	$self->{win_obj} = $win;
+	
+	$self->refresh;
 	return $self;
+}
+
+#----------------------#
+#   強調リストの更新   #
+
+sub refresh{
+	my $self = shift;
+	$self->list->delete('all');
+	my $row = 0;
+	my $h = mysql_exec->select("SELECT name, type FROM d_force",1)->hundle;
+	while (my $i = $h->fetch){
+		$self->list->add($row,-at => "$row");
+		$self->list->itemCreate($row,0,-text => Jcode->new($i->[0])->sjis);
+		$self->list->itemCreate($row,1,-text => Jcode->new($i->[1])->sjis);
+		++$row;
+	}
+	
+}
+
+
+#--------------#
+#   アクセサ   #
+#--------------#
+
+sub list{
+	my $self = shift;
+	return $self->{list};
 }
 
 sub win_name{
