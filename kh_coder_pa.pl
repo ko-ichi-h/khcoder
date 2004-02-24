70def5117921ac97a8283d27f1677156fcd6c275
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2004-02-24 15:31:31 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@204 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_coder_pa.pl
+++ kh_coder_pa.pl
@@ -35,22 +35,9 @@ $config_obj = kh_sysconfig->readin('./config/coder.ini',&cwd);
 $main_gui = gui_window::main->open;
 MainLoop;
 
+#-----------------#
+#   for PerlApp   #
 
-#--------------------#
-#   テスト用コード   #
-
-# テスト用プロジェクトを開く
-#kh_project->temp(
-#	target  =>
-#		'F:/home/Koichi/Study/perl/test_data/kokoro/kokoro.txt',
-#	dbname  =>
-#		'khc4',
-#)->open;
-#$::main_gui->close_all;
-#$::main_gui->menu->refresh;
-#$::main_gui->inner->refresh;
-
-# 特定の（テスト用）Windowを開く
-#gui_window::word_ass->open;
-#
-#MainLoop;
+use Tk::DragDrop::Win32Drop;
+use Tk::DragDrop::Win32Site;
+use SQL::Dialects::CSV;
