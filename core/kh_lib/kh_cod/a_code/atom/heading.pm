c6bcbe8f0abf96146989c6c6761a6da95093a24a
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2004-03-15 20:11:10 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@215 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/kh_cod/a_code/atom/heading.pm
+++ core/kh_lib/kh_cod/a_code/atom/heading.pm
@@ -4,13 +4,20 @@ package kh_cod::a_code::atom::heading;
 use base qw(kh_cod::a_code::atom);
 use strict;
 
+my $num = 0;
+sub reset{
+	$num = 0;
+}
+
 sub expr{
 	my $self = shift;
-	if ($self->{valid}){
-		return " ( $self->{tani}.$self->{heading_tani}"."_id = $self->{heading_id} ) ";
-	} else {
-		return 0;
-	}
+	my $t = $self->tables;
+	unless ($t){ return '0';}
+	
+	$t = $t->[0];
+	my $col = (split /\_/, $t)[2].(split /\_/, $t)[3];
+	my $sql = "IFNULL(".$self->parent_table.".$col,0)";
+	return $sql;
 }
 
 sub num_expr{
@@ -27,6 +34,7 @@ sub ready{
 	if ($self->raw =~ /<>見出し([1-5])\-\->(.+)$/o){
 		$var = $1;
 		$val = $2;
+		$val = '\'<h'."$var".'>'."$val".'</h'."$var".'>\'';
 		$self->{heading_tani} = "h"."$var";
 	} else {
 		die("something wrong!");
@@ -41,30 +49,80 @@ sub ready{
 		}
 	}
 	
-	# 見出しIDを取得
-	my $temp = "h"."$var"."_id";
-	$val = '\'<h'."$var".'>'."$val".'</h'."$var".'>\'';
-	my $h = mysql_exec->select("
-		SELECT $temp
+	# 条件に合致する見出し文のリストを作製
+	my ($temp_s, $temp_c);
+	foreach my $i (1,2,3,4,5){
+		$temp_s .= "h"."$i"."_id,";
+		$temp_c .= "h"."$i"."_id int,\n";
+		last if $var == $i;
+	}
+	chop $temp_s; chop $temp_c; chop $temp_c;
+	mysql_exec->drop_table('ct_tmp_midashi'); # TEMPORARY 
+	mysql_exec->do("
+		CREATE TABLE ct_tmp_midashi (
+			$temp_c
+		) TYPE=HEAP
+	",1);
+	mysql_exec->do("
+		INSERT INTO ct_tmp_midashi ($temp_s)
+		SELECT $temp_s
 		FROM   bun, bun_r
 		WHERE
 			    bun.id = bun_r.id
 			AND bun_id = 0
 			AND dan_id = 0
 			AND rowtxt = $val
-	",1)->hundle;
-	unless ($val = $h->fetch){
-			$self->{valid} = 0;
-			return;
+	",1);
+	
+	# 当該の見出し文を持つケースをリストアップ
+
+	my $table = "ct_$tani"."_heading_$num";
+	$self->{tables} = ["$table"];
+	++$num;
+	mysql_exec->drop_table($table);
+	mysql_exec->do("
+		CREATE TABLE $table (
+			id INT primary key not null,
+			num INT
+		)
+	",1);
+	my $sql;
+	$sql .= "INSERT INTO $table (id, num)\n";
+	$sql .= "SELECT $tani.id, 1\n";
+	$sql .= "FROM $tani, ct_tmp_midashi\n";
+	$sql .= "WHERE\n";
+	my $n = 0;
+	foreach my $i (1,2,3,4,5){
+		$sql .= "AND " if $n;
+		$sql .= "$tani.h"."$i"."_id = ct_tmp_midashi.h"."$i"."_id\n";
+		last if $var == $i;
+		++$n;
 	}
-	$self->{heading_id} = $val->[0];
-	print "h_id: $self->{heading_id}\n";
+	mysql_exec->do($sql,1);
+	
 	return $self;
 }
 
+#-------------------------------#
+#   利用するtmp tableのリスト   #
+
 sub tables{
-	return 0;
+	my $self = shift;
+	return $self->{tables};
 }
+
+#----------------#
+#   親テーブル   #
+sub parent_table{
+	my $self = shift;
+	my $new  = shift;
+	
+	if (length($new)){
+		$self->{parent_table} = $new;
+	}
+	return $self->{parent_table};
+}
+
 
 sub pattern{
 	return '^<>見出し[1-5]\-\->.+';
