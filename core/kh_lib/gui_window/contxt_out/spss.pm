3cd66818dc260dbf8a9ec11f9483f97a86f95a75
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2004-03-15 21:04:41 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@216 ab1598f5-0ef3-491f-8894-8c1f2390833e

---
+++ core/kh_lib/gui_window/contxt_out/spss.pm
@@ -0,0 +1,59 @@
+package gui_window::contxt_out::spss;
+use base qw(gui_window::contxt_out);
+
+use strict;
+
+#--------------#
+#   ロジック   #
+#--------------#
+
+sub go{
+	print "go!";
+	
+	my $self = shift;
+	my $file = shift;
+	
+	mysql_contxt->new(
+		tani    => $self->{tani_obj}->value,
+		hinshi2 => $self->hinshi2,
+		max2    => $self->max2,
+		min2    => $self->min2,
+		hinshi  => $self->hinshi,
+		max     => $self->max,
+		min     => $self->min,
+	)->culc->save($file);
+	
+}
+
+#-----------------#
+#   保存先の参照  #
+
+sub file_name{
+	my $self = shift;
+	my @types = (
+		[ "spss syntax file",[qw/.sps/] ],
+		["All files",'*']
+	);
+	my $path = $self->win_obj->getSaveFile(
+		-defaultextension => '.sps',
+		-filetypes        => \@types,
+		-title            =>
+			Jcode->new('「抽出語ｘ文脈ベクトル」表：名前を付けて保存')->sjis,
+		-initialdir       => $::config_obj->cwd
+	);
+	unless ($path){
+		return 0;
+	}
+	return $path;
+}
+
+# Windowラベル
+sub label{
+	return '「抽出語ｘ文脈ベクトル」表の出力： SPSS';
+}
+
+sub win_name{
+	return 'w_cross_out_spss';
+}
+
+1;
