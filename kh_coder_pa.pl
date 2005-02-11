7a0b1f4384caf39013d9bbc6b2111d38cddd2599
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2005-02-11 02:51:40 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@267 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_coder_pa.pl
+++ kh_coder_pa.pl
@@ -6,7 +6,7 @@ use vars qw($config_obj $project_obj $main_gui $splash $kh_version);
 BEGIN {
 	$kh_version = "2.alpha.6";
 	use Cwd qw(cwd);
-	push @INC, cwd.'/kh_lib';
+	use lib cwd.'/kh_lib';
 	if ($^O eq 'MSWin32'){
 		require Tk::Splash;
 		$splash = Tk::Splash->Show(
@@ -18,6 +18,9 @@ BEGIN {
 	} else {
 		push @INC, cwd.'/dummy_lib';
 	}
+	
+	use kh_sysconfig;
+	$config_obj = kh_sysconfig->readin('./config/coder.ini',&cwd);
 }
 
 use Tk;
@@ -28,10 +31,8 @@ use mysql_conc;
 use kh_project;
 use kh_projects;
 use kh_morpho;
-use kh_sysconfig;
 use gui_window;
 
-$config_obj = kh_sysconfig->readin('./config/coder.ini',&cwd);
 $main_gui = gui_window::main->open;
 MainLoop;
 
