5696d43d6596a51e966b8d79460e20a62c6ae0af
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2005-11-25 11:11:15 +0000

    MODs for automatic testing
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@375 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_lib/gui_hlist.pm
+++ kh_lib/gui_hlist.pm
@@ -6,6 +6,7 @@ use gui_hlist::linux;
 
 # Usage:
 # 	gui_hlist->copy($hlist_obj);
+# 	gui_hlist->copy($get_all);
 
 sub copy{
 	my $class = shift;
@@ -13,6 +14,27 @@ sub copy{
 	$self->{list} = shift;
 	bless $self, "$class"."::".$::config_obj->os;
 	$self->_copy;
+}
+
+sub get_all{
+	my $class = shift;
+	my $self;
+	$self->{list} = shift;
+	bless $self, "$class"."::".$::config_obj->os;
+	
+	# —ñ”
+	my $cols = pop @{$self->list->configure(-columns)}; --$cols;
+	
+	my $t = '';
+	my $n = 0;
+	while ($self->list->info('exists', $n)){
+		for (my $c = 0; $c <= $cols; ++$c){
+			$t .= $self->list->itemCget($n, $c, -text)."\t";
+		}
+		chop $t; $t .= "\n";
+		++$n;
+	}
+	return $t;
 }
 
 sub list{
