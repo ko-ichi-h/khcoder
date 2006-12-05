867ee69d85a07e18967b347da996f47387f892e3
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2006-12-05 15:43:22 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@499 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- auto_test/lib/kh_at/out_var.pm
+++ auto_test/lib/kh_at/out_var.pm
@@ -13,8 +13,40 @@ sub _exec_test{
 	$win->_read;
 
 	# 読み込み結果のチェック
+	$self->{result} .= "■読み込み結果\n";
+	$win = gui_window::outvar_list->open;
+	$self->{result} .= Jcode->new(
+		gui_window->gui_jg( gui_hlist->get_all( $win->{list} ) )
+	)->euc;
 
+	# 外部変数を1つ削除
+	$win->{list}->selectionSet(1);
+	$win->_delete(
+		no_conf => 1,
+	);
+	$self->{result} .= "■変数削除の結果\n";
+	$self->{result} .= Jcode->new(
+		gui_window->gui_jg( gui_hlist->get_all( $win->{list} ) )
+	)->euc;
 
+	# ラベル編集(1)
+	$win->{list}->selectionSet(1);
+	$win->_open_var;
+	my $win_edit = $::main_gui->get('w_outvar_detail');
+	$win_edit->{entry}{0}->insert(0, gui_window->gui_jchar('なし') );
+	$win_edit->{entry}{1}->insert(0, gui_window->gui_jchar('あり') );
+	$win_edit->_save;
+
+	# ラベル編集(2)
+	$win->{list}->selectionSet(0);
+	$win->_open_var;
+	my $win_edit = $::main_gui->get('w_outvar_detail');
+	$win_edit->{entry}{1}->insert(0, gui_window->gui_jchar('上') );
+	$win_edit->{entry}{2}->insert(0, gui_window->gui_jchar('中') );
+	$win_edit->{entry}{3}->insert(0, gui_window->gui_jchar('下') );
+	$win_edit->_save;
+
+	# ラベル編集の結果は、コーディング結果からチェックする…。
 
 	return $self;
 }
