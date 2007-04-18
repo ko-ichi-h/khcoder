1a977d2759c65b7f0905d5a08d666b6d860c61ec
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2007-04-18 07:26:26 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@504 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- auto_test.pl
+++ auto_test.pl
@@ -33,6 +33,19 @@ use kh_projects;
 use kh_morpho;
 use gui_window;
 
+# Rの初期化
+use Statistics::R;
+$::config_obj->{R} = Statistics::R->new(
+	log_dir => $::config_obj->{cwd}.'/config/R-bridge'
+);
+if ($::config_obj->{R}){
+	$::config_obj->{R}->startR;
+	$::config_obj->{R}->output_chk(1);
+} else {
+	$::config_obj->{R} = 0;
+}
+chdir ($::config_obj->{cwd});
+
 $main_gui = gui_window::main->open;
 
 #--------------------#
@@ -47,16 +60,16 @@ print "Starting test procedures...\n";
 open (STDOUT,">stdout.txt") or die;
 my $t0 = new Benchmark;
 
-kh_at::project_new->exec_test('project_new');      # テストファイル登録&前処理
-#kh_at->open_test_project;
+#kh_at::project_new->exec_test('project_new');      # テストファイル登録&前処理
+kh_at->open_test_project;
 
-kh_at::pretreatment->exec_test('pretreatment');    # 前処理メニュー
-kh_at::words->exec_test('words');                  # 抽出語メニュー
-kh_at::out_var->exec_test('out_var');              # 外部変数メニュー
+#kh_at::pretreatment->exec_test('pretreatment');    # 前処理メニュー
+#kh_at::words->exec_test('words');                  # 抽出語メニュー
+#kh_at::out_var->exec_test('out_var');              # 外部変数メニュー
 kh_at::cod->exec_test('cod');                      # コーディング
 
-kh_at->close_test_project;                         # プロジェクトを閉じる
-kh_at->delete_test_project;                        # プロジェクトを削除
+#kh_at->close_test_project;                         # プロジェクトを閉じる
+#kh_at->delete_test_project;                        # プロジェクトを削除
 
 my $t1 = new Benchmark;
 
--- auto_test/lib/kh_at/cod.pm
+++ auto_test/lib/kh_at/cod.pm
@@ -13,26 +13,85 @@ sub _exec_test{
 	$win->{tani_obj}->{raw_opt} = 'bun';
 	$win->{tani_obj}->mb_refresh;
 	$win->_calc;
-	$self->{result} .= "■単純集計・文単位\n";
+	$self->{result} .= "■単純集計：文単位\n";
 	$self->{result} .= Jcode->new(
 		gui_window->gui_jg( gui_hlist->get_all( $win->{list} ) )
 	)->euc;
 	$win->{tani_obj}->{raw_opt} = 'dan';
 	$win->{tani_obj}->mb_refresh;
 	$win->_calc;
-	$self->{result} .= "■単純集計・段落単位\n";
+	$self->{result} .= "■単純集計：段落単位\n";
 	$self->{result} .= Jcode->new(
 		gui_window->gui_jg( gui_hlist->get_all( $win->{list} ) )
 	)->euc;
 	$win->{tani_obj}->{raw_opt} = 'h2';
 	$win->{tani_obj}->mb_refresh;
 	$win->_calc;
-	$self->{result} .= "■単純集計・h2単位\n";
+	$self->{result} .= "■単純集計：h2単位\n";
 	$self->{result} .= Jcode->new(
 		gui_window->gui_jg( gui_hlist->get_all( $win->{list} ) )
 	)->euc;
 
-
+	# 章・節・段落ごとの集計
+	my $win1 = gui_window::cod_tab->open;
+	$win1->{tani_obj}->{raw_opt} = gui_window->gui_jchar('段落','euc');
+	$win1->{tani_obj}->check;
+	$win1->{tani_obj}->{raw_opt2} = gui_window->gui_jchar('H1','euc');
+	$win1->{tani_obj}->{opt2}->update;
+	$win1->_calc;
+	$self->{result} .= "■章・節・段落ごと：段落―H1単位\n";
+	my $t = '';
+	foreach my $i (@{$win1->{result}}){
+		my $n = 0;
+		foreach my $h (@{$i}){
+			$t .= "\t" if $n;
+			$t .= $h;
+			++$n;
+		}
+		$t .= "\n";
+	}
+	$t = Jcode->new($t)->euc;
+	$self->{result} .= $t;
+	
+	$win1->{tani_obj}->{raw_opt} = gui_window->gui_jchar('H2','euc');
+	$win1->{tani_obj}->check;
+	$win1->{tani_obj}->{raw_opt2} = gui_window->gui_jchar('H1','euc');
+	$win1->{tani_obj}->{opt2}->update;
+	$win1->_calc;
+	$self->{result} .= "■章・節・段落ごと：H2―H1単位\n";
+	$t = '';
+	foreach my $i (@{$win1->{result}}){
+		my $n = 0;
+		foreach my $h (@{$i}){
+			$t .= "\t" if $n;
+			$t .= $h;
+			++$n;
+		}
+		$t .= "\n";
+	}
+	$t = Jcode->new($t)->euc;
+	$self->{result} .= $t;
+	
+	$win1->{tani_obj}->{raw_opt} = gui_window->gui_jchar('文','euc');
+	$win1->{tani_obj}->check;
+	$win1->{tani_obj}->{raw_opt2} = gui_window->gui_jchar('H2','euc');
+	$win1->{tani_obj}->{opt2}->update;
+	$win1->_calc;
+	$self->{result} .= "■章・節・段落ごと：文―H2単位\n";
+	$t = '';
+	foreach my $i (@{$win1->{result}}){
+		my $n = 0;
+		foreach my $h (@{$i}){
+			$t .= "\t" if $n;
+			$t .= $h;
+			++$n;
+		}
+		$t .= "\n";
+	}
+	$t = Jcode->new($t)->euc;
+	$self->{result} .= $t;
+	
+	
 	return $self;
 }
 
