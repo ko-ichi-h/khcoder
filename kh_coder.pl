5b981c896c46b4708cc131ae14f4afbfd15a20ce
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2008-04-24 11:16:31 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@569 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_coder.pl
+++ kh_coder.pl
@@ -18,7 +18,7 @@ use strict;
 use vars qw($config_obj $project_obj $main_gui $splash $kh_version);
 
 BEGIN {
-	$kh_version = "2.beta.12pre";
+	$kh_version = "2.beta.12";
 	use Cwd qw(cwd);
 	use lib cwd.'/kh_lib';
 	use lib cwd.'/plugin';
