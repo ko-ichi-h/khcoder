f9ba2b2f85eec2ce3df96d5c221ad79293efa9bc
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2008-08-21 11:09:56 +0000

    (none)
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@626 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/kh_r_plot.pm
+++ core/kh_lib/kh_r_plot.pm
@@ -55,6 +55,9 @@ sub save{
 	elsif ($path =~ /\.eps$/i){
 		$self->_save_eps($path);
 	}
+	elsif ($path =~ /\.pdf$/i){
+		$self->_save_pdf($path);
+	}
 	elsif ($path =~ /\.emf$/i){
 		$self->_save_emf($path);
 	}
@@ -81,6 +84,26 @@ sub _save_emf{
 	return 1;
 }
 
+sub _save_pdf{
+	my $self = shift;
+	my $path = shift;
+	
+	# ƒvƒƒbƒgì¬
+	$::config_obj->R->output_chk(0);
+	$::config_obj->R->lock;
+	$::config_obj->R->send(
+		 "pdf(file=\"$path\", height = 7, width = 7,"
+		."family=\"Japan1GothicBBB\")"
+	);
+	$::config_obj->R->send($self->{command_f});
+	$::config_obj->R->send('dev.off()');
+	$::config_obj->R->unlock;
+	$::config_obj->R->output_chk(1);
+	
+	return 1;
+}
+
+
 sub _save_eps{
 	my $self = shift;
 	my $path = shift;
@@ -90,7 +113,8 @@ sub _save_eps{
 	$::config_obj->R->lock;
 	$::config_obj->R->send(
 		 "postscript(\"$path\", horizontal = FALSE, onefile = FALSE,"
-		."paper = \"special\", height = 7, width = 7 )"
+		."paper = \"special\", height = 7, width = 7,"
+		."family=\"Japan1GothicBBB\" )"
 	);
 	$::config_obj->R->send($self->{command_f});
 	$::config_obj->R->send('dev.off()');
@@ -131,5 +155,82 @@ sub _save_r{
 	return 1;
 }
 
+sub _pdf_font {
+	my $cmd = '
+postscriptFonts(Japan1 = CIDFont("HeiseiKakuGo-W5", "90ms-RKSJ-H", "cp932"),
+                Japan1HeiMin = CIDFont("HeiseiMin-W3", "90ms-RKSJ-H", "cp932"),
+                Japan1GothicBBB =
+                CIDFont("GothicBBB-Medium", "90ms-RKSJ-H", "cp932"),
+                Japan1Ryumin = CIDFont("Ryumin-Light", "90ms-RKSJ-H", "cp932"))
+
+pdfFonts(Japan1 = CIDFont("KozMinPro-Regular-Acro", "90ms-RKSJ-H", "cp932",
+           paste("/FontDescriptor",
+                 "<<",
+                 "  /Type /FontDescriptor",
+                 "  /CapHeight 740 /Ascent 1075 /Descent -272 /StemV 72",
+                 "  /FontBBox [-195 -272 1110 1075]",
+                 "  /ItalicAngle 0 /Flags 6 /XHeight 502",
+                 "  /Style << /Panose <000001000500000000000000> >>",
+                 ">>",
+                 "/CIDSystemInfo << /Registry(Adobe) /Ordering(Japan1) /Supplement  2 >>",
+                 "/DW 1000",
+                 "/W [",
+                 "   1 632 500 ",
+                 "   8718 [500 500] ",
+                 "]\n",
+                 sep="\n      ")),
+         Japan1HeiMin = CIDFont("HeiseiMin-W3-Acro", "90ms-RKSJ-H", "cp932",
+           paste("/FontDescriptor",
+                 "<<",
+                 "  /Type /FontDescriptor",
+                 "  /CapHeight 709 /Ascent 723 /Descent -241 /StemV 69",
+                 "  /FontBBox [-123 -257 1001 910]",
+                 "  /ItalicAngle 0 /Flags 6 /XHeight 450",
+                 "  /Style << /Panose <000002020500000000000000> >>",
+                 ">>",
+                 "/CIDSystemInfo << /Registry(Adobe) /Ordering(Japan1) /Supplement  2 >>",
+                 "/DW 1000",
+                 "/W [",
+                 "   1 632 500 ",
+                 "   8718 [500 500] ",
+                 "]\n",
+                 sep="\n      ")),
+         Japan1GothicBBB = CIDFont("GothicBBB-Medium", "90ms-RKSJ-H", "cp932",
+           paste("/FontDescriptor",
+                 "<<",
+                 "  /Type /FontDescriptor",
+                 "  /CapHeight 737 /Ascent 752 /Descent -271 /StemV 99",
+                 "  /FontBBox [-22 -252 1000 892]",
+                 "  /ItalicAngle 0 /Flags 4",
+                 "  /Style << /Panose <0801020b0500000000000000> >>",
+                 ">>",
+                 "/CIDSystemInfo << /Registry(Adobe) /Ordering(Japan1) /Supplement  2 >>",
+                 "/DW 1000",
+                 "/W [",
+                 "   1 632 500",
+                 "   8718 [500 500]",
+                 "]\n",
+                 sep="\n      ")),
+         Japan1Ryumin = CIDFont("Ryumin-Light", "90ms-RKSJ-H", "cp932",
+           paste("/FontDescriptor",
+                 "<<",
+                 "  /Type /FontDescriptor",
+                 "  /CapHeight 709 /Ascent 723 /Descent -241 /StemV 69",
+                 "  /FontBBox [-54 -305 1000 903]",
+                 "  /ItalicAngle 0 /Flags 6",
+                 "  /Style << /Panose <010502020300000000000000> >>",
+                 ">>",
+                 "/CIDSystemInfo << /Registry(Adobe) /Ordering(Japan1) /Supplement  2 >>",
+                 "/DW 1000",
+                 "/W [",
+                 "   1 632 500",
+                 "   8718 [500 500]",
+                 "]\n",
+                 sep="\n      ")))
+}
+	';
+	$::config_obj->R->send($cmd);
+
+}
 
 1;
