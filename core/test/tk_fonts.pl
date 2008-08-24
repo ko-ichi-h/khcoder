f5b48aec1606629269bd6dd1ce45c592a4dee38e
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2008-08-24 13:50:27 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@633 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/test/tk_fonts.pl
+++ core/test/tk_fonts.pl
@@ -10,21 +10,39 @@ my $self;
 $self->{mw} = MainWindow->new;
 bless $self, 'FontTest';
 
+my $mw = $self->{mw};
+
 $self->{mw}->fontCreate('TKFN',
 	-family => 'HiraginoKaku',
 	-size   => 10
 );
 $self->{mw}->optionAdd('*font', "TKFN");
 
-$self->{mw}->Label(
-	-text => $self->gui_jchar('日本語のラベル'),
-	-font => "TKFN"
-)->pack();
-$self->{mw}->Button(
-	-text => $self->gui_jchar('Font変更'),
-	-font => "TKFN",
-	-command => sub {$self->font_change},
-)->pack;
+#$self->{mw}->Label(
+#	-text => $self->gui_jchar('日本語のラベル'),
+#	-font => "TKFN"
+#)->pack();
+#$self->{mw}->Button(
+#	-text => $self->gui_jchar('Font変更'),
+#	-font => "TKFN",
+#	-command => sub {$self->font_change},
+#)->pack;
+
+$mw->Button(-text => "Button1", -command => sub { exit })->grid
+  ($mw->Button(-text => "Button2", -command => sub { exit }),
+   $mw->Button(-text => "Button3", -command => sub { exit }),
+   $mw->Button(-text => "Button4", -command => sub { exit }),
+   -sticky => "nsew");
+ 
+$mw->Button(-text => "Button5", -command => sub { exit })->grid
+  ("x",
+   $mw->Button(-text => "Button7", -command => sub { exit }),
+   $mw->Button(-text => "Button8", -command => sub { exit }),
+   -sticky => "nsew");
+ 
+#$mw->gridColumnconfigure(1, -weight => 1);
+#$mw->gridRowconfigure(1, -weight => 1);
+
 
 
 MainLoop;
