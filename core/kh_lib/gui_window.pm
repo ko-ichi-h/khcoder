bb93df7728a7a91f8bf9a83991d0b0b083face3e
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2008-11-23 12:31:33 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@733 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/gui_window.pm
+++ core/kh_lib/gui_window.pm
@@ -33,6 +33,7 @@ use gui_window::word_tf_df;
 use gui_window::word_corresp;
 use gui_window::word_cls;
 use gui_window::word_mds;
+use gui_window::word_netgraph;
 use gui_window::doc_view;
 use gui_window::doc_search;
 use gui_window::doc_cls;
