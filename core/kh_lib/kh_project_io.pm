05663f99bf9f693b5b3b1c5fca4f5cf1c1a70055
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2009-04-21 08:25:50 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@781 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/kh_project_io.pm
+++ core/kh_lib/kh_project_io.pm
@@ -1,6 +1,9 @@
 package kh_project_io;
-#use strict;
+use strict;
 use MySQL::Backup_kh;
+use MIME::Base64;
+use YAML::XS qw(DumpFile LoadFile);               # XSのままで良い？
+use Archive::Zip;
 
 sub export{
 	my $savefile = $_[0];
@@ -8,13 +11,7 @@ sub export{
 	my $mb = new_from_DBH MySQL::Backup_kh($::project_obj->dbh);
 
 	# MySQLのデータを格納
-	my $n = 0;
-	my $file_temp_mysql = 'mysql.tmp'.$n;
-	while (-e $file_temp_mysql){
-		++$n;
-		$file_temp_mysql = 'mysql.tmp'.$n;
-	}
-
+	my $file_temp_mysql = $::config_obj->file_temp;
 	open (MYSQLO,">$file_temp_mysql") or
 		gui_errormsg->open(
 			type => 'file',
@@ -31,7 +28,34 @@ sub export{
 	#    →primary key以外のindexは、データをinsertした後に作成するように
 	# 3. リストアするときもファイル全体を一気に読まず、1行づつに
 
+	# 情報ファイルを作成
+	my $file_temp_info = $::config_obj->file_temp;
+	my %info;
+	$info{'file_name'} = encode_base64($::project_obj->file_short_name,'');
+	$info{'comment'}   = encode_base64($::project_obj->comment,'');
+	DumpFile($file_temp_info, %info) or
+		gui_errormsg->open(
+			type => 'file',
+			file => $file_temp_info
+		)
+	;
 
+	# Zipファイルに固める
+	my $zip = Archive::Zip->new();
+	
+	$zip->addFile( $file_temp_mysql, 'mysql' );
+	$zip->addFile( $file_temp_info,  'info' );
+	$zip->addFile( $::project_obj->file_target, 'target');
+	
+	unless ( $zip->writeToFileNamed($savefile) == "AZ_OK" ) {
+		gui_errormsg->open(
+			type => 'file',
+			file => $savefile
+		)
+	}
+
+	# 一時ファイルを削除
+	unlink ($file_temp_mysql, $file_temp_info);
 
 	print "OK\n";
 }
