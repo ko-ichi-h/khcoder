8bd31a72ec0f1f2ad390a26532b07b0259965d9e
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2009-08-28 10:18:43 +0000

    Implementing naive bayes classifier.
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@838 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/kh_nbayes/predict.pm
+++ core/kh_lib/kh_nbayes/predict.pm
@@ -4,7 +4,6 @@ use base qw(kh_nbayes);
 
 use strict;
 
-
 sub each{
 	my $self = shift;
 	my $current = shift;
@@ -43,7 +42,91 @@ sub each{
 	return $self;
 }
 
+sub make_log_file{
+	my $self = shift;
 
+	my $fixer = 0;
+	foreach my $i (values %{$self->{cls}{model}{smoother}}){
+		#print "fx: $i\n";
+		$fixer = $i if $fixer > $i;
+	}
+	my @labels = $self->{cls}->labels;
+	
+	open (LOUT,">$self->{save_path}") or 
+		gui_errormsg->open(
+			type    => 'file',
+			thefile => "$self->{save_path}",
+		);
+	
+	
+	# $i = 文書 No.
+	foreach my $i (sort {$a <=> $b} keys %{$self->{result_log}} ){
+		print LOUT "文書 No. $i\n\n";
+
+		# 「各抽出語のスコア」表を作成
+		my ($rows, $scores) =
+			&make_each_log_table(
+				$self->{result_log}{$i},      # ログデータ
+				\@labels,                     # カテゴリのカラム順指定
+				$fixer,                       # min( smoothers )
+			);
+
+		print LOUT "スコア：\n";
+		my ($max, $max_ord, $n) = (0, 0, 0);
+		foreach my $h (@labels){
+			print LOUT "\t$h\t$scores->{$h}\n";
+			if ($max < $scores->{$h}){
+				$max = $scores->{$h};
+				$max_ord = $n;
+			}
+			++$n;
+		}
+		$max_ord += 2;
+
+		print LOUT "\n各抽出語のスコア：\n";
+		print LOUT "\t抽出語\t頻度";
+		foreach my $h (@labels){
+			print LOUT "\t$h";
+		}
+		print LOUT "\n";
+
+		my $tt = '';
+		foreach my $h (sort {$b->[$max_ord] <=> $a->[$max_ord]} @{$rows}){
+			my $t = "\t";
+			foreach my $k (@{$h}){
+				$t .= "$k\t";
+			}
+			chop $t;
+			$tt .= "$t\n";
+		}
+		print LOUT "$tt";
+		print LOUT "-------------------------------------------------------------------------------\n\n"
+	}
+	close (LOUT);
+	kh_jchar->to_sjis($self->{save_path});
+	return 1;
+}
+
+
+sub make_each_log_table{
+	my $d      = shift;
+	my $labels = shift;
+	my $fixer  = shift;
+	
+	my @rows;
+	my %scores;
+	# $h = 抽出語
+	foreach my $h (keys %{$d} ){
+		my $current = [$h, $d->{$h}{v}];
+		foreach my $j (@{$labels}){
+			push @{$current}, ( $d->{$h}{l}{$j} - $fixer ) * $d->{$h}{v};
+			$scores{$j} +=    ( $d->{$h}{l}{$j} - $fixer ) * $d->{$h}{v};
+		}
+		push @rows, $current;
+	}
+	
+	return (\@rows, \%scores);
+}
 
 
 
