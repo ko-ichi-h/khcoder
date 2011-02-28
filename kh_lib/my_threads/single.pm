89e28e536ffcec73d26964bb244c391f424d659e
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2011-02-28 17:22:37 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@1096 ab1598f5-0ef3-491f-8894-8c1f2390833e

---
+++ kh_lib/my_threads/single.pm
@@ -0,0 +1,46 @@
+package my_threads::single;
+
+use strict;
+use Exporter;
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw(&init &open_project &exec1 &exec2 &wait1 &wait2);
+
+sub init{
+	print "Using un-threaded functions...\n";
+	return 1;
+}
+
+sub open_project{
+	return 1;
+}
+
+sub exec1{
+	my $class = shift;
+	my $cmd   = shift;
+	eval( $cmd );
+	if ($@){
+		die("Error in the thread (s): $@\n");
+	}
+	return 1;
+}
+
+sub exec2{
+	my $class = shift;
+	my $cmd   = shift;
+	eval( $cmd );
+	if ($@){
+		die("Error in the thread (s): $@\n");
+	}
+	return 1;
+}
+
+sub wait1{
+	return 1;
+}
+
+sub wait2{
+	return 1;
+}
+
+1;
