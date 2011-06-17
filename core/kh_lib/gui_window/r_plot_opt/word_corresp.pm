f766ffe3bd3a090132440d200e1542c3e157471a
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2011-06-17 00:55:59 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@1181 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/gui_window/r_plot_opt/word_corresp.pm
+++ core/kh_lib/gui_window/r_plot_opt/word_corresp.pm
@@ -76,8 +76,9 @@ sub innner{
 	)->pack(-side => 'left');
 	#$self->refresh_flt;
 
+	# バブルプロット関連
 	$lf->Checkbutton(
-		-text     => $self->gui_jchar('語の出現数を円の大きさで表現（バブル）'),
+		-text     => $self->gui_jchar('出現数の多い語ほど大きく描画（バブルプロット）'),
 		-variable => \$self->{check_bubble},
 		-command  => sub{ $self->refresh_std_radius;},
 	)->pack(
@@ -94,11 +95,19 @@ sub innner{
 		-text => '  ',
 		-font => "TKFN",
 	)->pack(-anchor => 'w', -side => 'left');
-	
+
+	$self->{chkw_resize_vars} = $frm_std_radius->Checkbutton(
+			-text     => $self->gui_jchar('変数の値 / 見出しの大きさも可変に','euc'),
+			-variable => \$self->{chk_resize_vars},
+			-anchor => 'w',
+			-state => 'disabled',
+	)->pack(-anchor => 'w');
+
 	$self->{chkw_std_radius} = $frm_std_radius->Checkbutton(
-			-text     => $self->gui_jchar('円の大きさを標準化','euc'),
+			-text     => $self->gui_jchar('バブルの大きさを標準化する','euc'),
 			-variable => \$self->{chk_std_radius},
 			-anchor => 'w',
+			-state => 'disabled',
 	)->pack(-anchor => 'w');
 
 	# 成分
@@ -181,6 +190,13 @@ sub innner{
 	} else {
 		$self->{chk_std_radius} = 1;
 	}
+	
+	if ( $self->{command_f} =~ /resize_vars <\- ([0-9]+)\n/ ){
+		$self->{chk_resize_vars} = $1;
+	} else {
+		$self->{chk_resize_vars} = 1;
+	}
+	
 	$self->refresh_std_radius;
 
 
@@ -220,8 +236,10 @@ sub refresh_std_radius{
 	my $self = shift;
 	if ( $self->{check_bubble} ){
 		$self->{chkw_std_radius}->configure(-state => 'normal');
+		$self->{chkw_resize_vars}->configure(-state => 'normal');
 	} else {
 		$self->{chkw_std_radius}->configure(-state => 'disabled');
+		$self->{chkw_resize_vars}->configure(-state => 'disabled');
 	}
 }
 
@@ -275,6 +293,7 @@ sub calc{
 		plotwin_name => 'word_corresp',
 		bubble       => $self->gui_jg( $self->{check_bubble} ),
 		std_radius   => $self->gui_jg( $self->{chk_std_radius} ),
+		resize_vars  => $self->gui_jg( $self->{chk_resize_vars} ),
 	);
 	
 	$wait_window->end(no_dialog => 1);
