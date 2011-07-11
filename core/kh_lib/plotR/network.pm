65faef635517b8798194024cf79142e45d6cec65
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2011-07-11 15:43:54 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@1229 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/plotR/network.pm
+++ core/kh_lib/plotR/network.pm
@@ -451,8 +451,8 @@ if (com_method == "twomode_c"){
 
 	ccol[var_select] <- "#FB8072" # #FB8072 #DEEBF7 #FF9966 #FFDAB9 "#F46D43"
 
-	com_col_v <- "gray40"
-	edg_col   <- "gray65"
+	com_col_v <- "gray65"
+	edg_col   <- "gray70"
 	edg_lty   <- 1
 
 }
@@ -463,6 +463,10 @@ if (com_method == "none" || com_method == "twomode_g"){
 	com_col_v <- "black"
 	edg_lty <- 1
 	edg_col   <- "gray40"
+}
+
+if (com_method == "twomode_g"){
+	edg_lty <- 3
 }
 
 ';
@@ -734,6 +738,73 @@ if ( exists("target_words") ){
 		)
 	}
 }
+
+
+if(0){
+if (com_method == "twomode_g"){
+# 枠付きプロット関数の設定
+s.label_my <- function (dfxy, xax = 1, yax = 2, label = row.names(dfxy),
+    clabel = 1, 
+    pch = 20, cpoint = if (clabel == 0) 1 else 0, boxes = TRUE, 
+    neig = NULL, cneig = 2, xlim = NULL, ylim = NULL, grid = TRUE, 
+    addaxes = TRUE, cgrid = 1, include.origin = TRUE, origin = c(0, 
+        0), sub = "", csub = 1.25, possub = "bottomleft", pixmap = NULL, 
+    contour = NULL, area = NULL, add.plot = FALSE) 
+{
+    dfxy <- data.frame(dfxy)
+    opar <- par(mar = par("mar"))
+    on.exit(par(opar))
+    par(mar = c(0.1, 0.1, 0.1, 0.1))
+    coo <- scatterutil.base(dfxy = dfxy, xax = xax, yax = yax, 
+        xlim = xlim, ylim = ylim, grid = grid, addaxes = addaxes, 
+        cgrid = cgrid, include.origin = include.origin, origin = origin, 
+        sub = sub, csub = csub, possub = possub, pixmap = pixmap, 
+        contour = contour, area = area, add.plot = add.plot)
+    if (!is.null(neig)) {
+        if (is.null(class(neig))) 
+            neig <- NULL
+        if (class(neig) != "neig") 
+            neig <- NULL
+        deg <- attr(neig, "degrees")
+        if ((length(deg)) != (length(coo$x))) 
+            neig <- NULL
+    }
+    if (!is.null(neig)) {
+        fun <- function(x, coo) {
+            segments(coo$x[x[1]], coo$y[x[1]], coo$x[x[2]], coo$y[x[2]], 
+                lwd = par("lwd") * cneig)
+        }
+        apply(unclass(neig), 1, fun, coo = coo)
+    }
+    if (clabel > 0) 
+        scatterutil.eti(coo$x, coo$y, label, clabel, boxes)
+    if (cpoint > 0 & clabel < 1e-06) 
+        points(coo$x, coo$y, pch = pch, cex = par("cex") * cpoint)
+    #box()
+    invisible(match.call())
+}
+library(ade4)
+
+s.label_my(
+	lay_f[var_select,],
+	xax=1,
+	yax=2,
+	label=colnames(d)[ as.numeric( get.vertex.attribute(n2,"name") ) ][var_select],
+	boxes=T,
+	clabel=0.8,
+	addaxes=F,
+	include.origin=F,
+	grid=F,
+	cpoint=0,
+	cneig=0,
+	cgrid=0,
+	add.plot=T,
+)
+}
+}
+
+
+
 }
 '
 }
