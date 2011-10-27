4a02e8686a9c8092e9a431a5090c088b8d8005ec
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2011-10-27 05:17:46 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@1290 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_lib/kh_jchar.pm
+++ kh_lib/kh_jchar.pm
@@ -8,7 +8,7 @@ if (eval 'require Encode::EUCJPMS'){
 	$char_code{sjis} = 'cp932';
 } else {
 	$char_code{euc}  = 'euc-jp';
-	$char_code{sjis} = 'shiftjis';
+	$char_code{sjis} = 'cp932';
 }
 
 BEGIN{
