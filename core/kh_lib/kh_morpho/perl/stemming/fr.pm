c8997ebe8ace57bf4627702116b04caebb17cbb9
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2011-10-31 05:33:41 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@1298 ab1598f5-0ef3-491f-8894-8c1f2390833e

---
+++ core/kh_lib/kh_morpho/perl/stemming/fr.pm
@@ -0,0 +1,45 @@
+package kh_morpho::perl::stemming::fr;
+use strict;
+use base qw( kh_morpho::perl::stemming );
+
+sub init{
+	my $self = shift;
+	
+	$self->{splitter} = Lingua::Sentence->new('fr');
+	$self->{stemmer}  = Lingua::Stem::Snowball->new(
+		lang     => 'fr',
+		encoding => 'UTF-8'
+	);
+	
+	return $self;
+}
+
+sub tokenize{
+	my $self = shift;
+	my $t    = shift;
+
+	# 文末処理
+	$t =~ s/(.+)(["|''|']{0,1}[\.|\!+|\?+|\!+\?|\?+\!+]["|''|']{0,1})\s*$/\1 \2/go;
+
+	# コンマ
+	$t =~ s/(\S),([\s|\Z])/\1 ,\2/go;
+
+	# ダブルクォートやカッコ類
+	$t =~ s/(''|``|"|\(|\)|\[|\]|\{|\})(\S)/\1 \2/go;
+	$t =~ s/(\S)(''|``|"|\(|\)|\[|\]|\{|\})/\1 \2/go;
+
+	# シングルクォート
+	$t =~ s/(\S)'([\s|\Z])/\1 '\2/go;
+	$t =~ s/(\s|^)'(\S)/\1' \2/go;
+
+	# フランス語特有 「l'語」「s'語」「c'語」「d'語」
+	$t =~ s/(\s|^)([l|s|c|d]')(\S)/\1\2 \3/gio;
+
+
+	my @words_hyoso = split / /, $t;
+
+	return(\@words_hyoso, undef);
+}
+
+
+1;
---
+++ core/kh_lib/kh_morpho/perl/stemming/nl.pm
@@ -0,0 +1,43 @@
+package kh_morpho::perl::stemming::nl;
+use strict;
+use base qw( kh_morpho::perl::stemming );
+
+sub init{
+	my $self = shift;
+	
+	$self->{icode} = kh_jchar->check_code($self->target,1);
+	
+	$self->{splitter} = Lingua::Sentence->new('nl');
+	$self->{stemmer}  = Lingua::Stem::Snowball->new(
+		lang     => 'nl',
+		encoding => 'UTF-8'
+	);
+	
+	return $self;
+}
+
+sub tokenize{
+	my $self = shift;
+	my $t    = shift;
+
+	# 文末処理
+	$t =~ s/(.+)(["|''|']{0,1}[\.|\!+|\?+|\!+\?|\?+\!+]["|''|']{0,1})\s*$/\1 \2/go;
+
+	# コンマ
+	$t =~ s/(\S),([\s|\Z])/\1 ,\2/go;
+
+	# ダブルクォートやカッコ類
+	$t =~ s/(''|``|"|\(|\)|\[|\]|\{|\})(\S)/\1 \2/go;
+	$t =~ s/(\S)(''|``|"|\(|\)|\[|\]|\{|\})/\1 \2/go;
+
+	# シングルクォート
+	$t =~ s/(\S)'([\s|\Z])/\1 '\2/go;
+	$t =~ s/(\s|^)'(\S)/\1' \2/go;
+
+	my @words_hyoso = split / /, $t;
+
+	return(\@words_hyoso, undef);
+}
+
+
+1;
