01d591a548881aaa7485dce2520e39cca66f58b7
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2012-01-02 05:47:52 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@1413 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_lib/gui_window/r_plot/word_netgraph.pm
+++ kh_lib/gui_window/r_plot/word_netgraph.pm
@@ -6,28 +6,28 @@ sub option1_options{
 	
 	if (@{$self->{plots}} == 2){
 		return [
-			'カラー',
-			'グレースケール',
+			kh_msg->get('gui_window::r_plot::word_netgraph->col'), # カラー
+			kh_msg->get('gui_window::r_plot::word_netgraph->gray'), # グレー
 		] ;
 	} else {
 		return [
-			'中心性（媒介）',
-			'中心性（次数）',
-			'中心性（固有ベクトル）',
-			'サブグラフ検出（媒介）',
-			'サブグラフ検出（modularity）',
-			'なし',
+			kh_msg->get('gui_window::r_plot::word_netgraph->cnt_b'), # 中心性（媒介）
+			kh_msg->get('gui_window::r_plot::word_netgraph->cnt_d'), # 中心性（次数）
+			kh_msg->get('gui_window::r_plot::word_netgraph->cnt_v'), # 中心性（固有ベクトル）
+			kh_msg->get('gui_window::r_plot::word_netgraph->com_b'), # サブグラフ検出（媒介）
+			kh_msg->get('gui_window::r_plot::word_netgraph->com_m'), # サブグラフ検出（modularity）
+			kh_msg->get('gui_window::r_plot::word_netgraph->none'),  # なし
 		];
 	}
 
 }
 
 sub option1_name{
-	return ' カラー：';
+	return kh_msg->get('gui_window::r_plot::word_netgraph->color'); #  カラー：
 }
 
 sub win_title{
-	return '抽出語・共起ネットワーク';
+	return kh_msg->get('win_title'); # 抽出語・共起ネットワーク
 }
 
 sub win_name{
