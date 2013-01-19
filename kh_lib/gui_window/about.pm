75d6fe23e0779c8db28301a45d4124e4601897b1
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2013-01-19 17:48:11 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@1616 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_lib/gui_window/about.pm
+++ kh_lib/gui_window/about.pm
@@ -183,7 +183,7 @@ sub _new{
 	}
 
 	$fra_r->Label(
-		-text => $self->gui_jchar($copy_mark.'2001-2012'),
+		-text => $self->gui_jchar($copy_mark.'2001-2013'),
 		-font => "TKFN",
 	)->pack(-anchor => 'nw', -pady=>'2', -side => 'left');
 
