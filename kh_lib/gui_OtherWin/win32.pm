20039436853c15748417e9023ecb55e2fd7b559d
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2013-04-02 18:44:50 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@1656 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_lib/gui_OtherWin/win32.pm
+++ kh_lib/gui_OtherWin/win32.pm
@@ -7,14 +7,28 @@ sub _open{
 	my $t = $self->target;
 	require Win32;
 	if ( Win32::IsWinNT() ){
-		my $cmd;
-		if ($t =~ /^http/o){
-			$cmd = "start /MIN cmd.exe /c start $t";
-		} else {
-			$cmd = "start /MIN cmd.exe /c \"\"$t\"\"";
-		}
-		system $cmd;
+	
+		### system関数（シンプル）
+		system("start \"khcoder\" \"$t\"");
+
+		### system関数
+		#my $cmd;
+		#if ($t =~ /^http/o){
+		#	$cmd = "start /MIN cmd.exe /c start $t";
+		#} else {
+		#	$cmd = "start /MIN cmd.exe /c \"\"$t\"\"";
+		#}
+		#system $cmd;
 		
+		### IPC::Run →perlappとの相性×
+		#if(defined(&PerlApp::extract_bound_file)){
+		#	my $path = $ENV{'PWD'}.'/'.PerlApp::extract_bound_file('perl.exe');
+		#	$path = Jcode->new($path,'sjis')->euc;
+		#	$path =~ s/\\/\//g;
+		#	$path = Jcode->new($path,'euc')->sjis;
+		#	$^X = $path;
+		#	print "perlapp: $path\n";
+		#}
 		#use IPC::Run qw( start pump );
 		#my ($in, $out,$err);
 		#my $h = start ["cmd.exe"], \$in , \$out, \$err;
@@ -24,6 +38,10 @@ sub _open{
 		#	$in = "\"\"$t\"\" & exit\n";
 		#}
 		#pump $h while length $in;
+
+		### IPC::System::Symple
+		#require IPC::System::Simple;
+		#IPC::System::Simple::system("start \"khcoder\" \"$t\"");
 
 	} else {
 		system "start command.com /c start \"$t\"";
