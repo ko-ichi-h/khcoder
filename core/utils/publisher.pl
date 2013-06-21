3e99f452cf9dea71e1b7d403f96563d35dc62e40
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2013-06-21 18:23:40 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@1671 ab1598f5-0ef3-491f-8894-8c1f2390833e

---
+++ core/utils/publisher.pl
@@ -0,0 +1,102 @@
+use strict;
+use Archive::Tar;
+use File::Find 'find';
+use File::Path 'rmtree';
+
+$Archive::Tar::DO_NOT_USE_PREFIX = 1;
+
+my $V = '2b30b';
+
+&source_tgz;
+
+
+sub source_tgz{
+	#---------------------------------#
+	#   CVSから最新ソースを取り出し   #
+
+	my $home_dir = '';
+	my $key_file = '';
+
+	# 家PC
+	if ( -e "f:/home/koichi/study/.ssh/id_dsa" ) {
+		$key_file = 'f:/home/koichi/study/.ssh/id_dsa';
+		$home_dir = 'f:/home/koichi';
+	}
+
+	my $cvs_cmd = 'cvs -d ":ext;command=\'';
+
+	if (-d $home_dir){
+		$cvs_cmd .= "set HOME=f:/home/koichi& ";
+	}
+
+	$cvs_cmd .= "ssh -l ko-ichi ";
+
+	if (-d $home_dir){
+		$cvs_cmd .= "-i $key_file ";
+	}
+
+	$cvs_cmd .= "khc.cvs.sourceforge.net':ko-ichi\@khc.cvs.sourceforge.net:/cvsroot/khc\" ";
+	$cvs_cmd .= "export -r HEAD -- core";
+
+	print "cmd: $cvs_cmd\n";
+
+	rmtree('core');
+	rmtree('kh_coder');
+	system($cvs_cmd);
+
+	#--------------------------#
+	#   不要なファイルを削除   #
+
+	my @rm_dir = (
+		'core/.settings',
+		'core/auto_test',
+		'core/test',
+		'core/utils',
+	);
+
+	my @rm_f = (
+		'core/memo/bib.html',
+		'core/memo/bib.tsv',
+		'core/memo/bib_t2h.pl',
+		'core/memo/bib_t2h.bat',
+		'core/memo/db_memo.csv',
+		'core/memo/devnote.txt',
+		'core/memo/performance.csv',
+		'core/plugin_jp/jssdb_bench1.pm',
+		'core/plugin_jp/jssdb_prepare.pm',
+		'core/plugin_jp/jssdb_search.pm',
+		'core/auto_test.pl',
+		'core/kh_coder.perlapp',
+		'core/make_exe.bat',
+	);
+
+	use File::Path 'rmtree';
+	foreach my $i (@rm_dir){
+		rmtree ($i) or warn("warn: could not delete: $i\n");
+	}
+
+	foreach my $i (@rm_f){
+		unlink ($i) or warn("warn: could not delete: $i\n");
+	}
+
+	#---------------------#
+	#   ソースZipを作成   #
+
+	unlink("khcoder-$V.tar.gz");
+
+	rename('core', 'kh_coder')
+		or warn("warn: could not rename core to kh_coder\n")
+	;
+
+	my @files;
+	find(
+		sub {
+			push @files, $File::Find::name;
+		},
+		"kh_coder"
+	);
+
+	my $tar = Archive::Tar->new;
+	$tar->add_files(@files);
+	$tar->write("khcoder-$V.tar.gz",9);
+}
