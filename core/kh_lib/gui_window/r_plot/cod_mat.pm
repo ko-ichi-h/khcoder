8d4b8a6691d809df9bba21abe6e87a13b068b483
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2013-07-30 08:03:16 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@1690 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/gui_window/r_plot/cod_mat.pm
+++ core/kh_lib/gui_window/r_plot/cod_mat.pm
@@ -18,6 +18,24 @@ sub photo_pane_width{
 	return 640;
 }
 
+# 画像表示用オブジェクトを再作成（スクロールバーをリセットするため）
+sub renew{
+	my $self = shift;
+	return 0 unless $self->{optmenu};
+
+	$gui_window::r_plot::imgs->{$self->win_name}->delete;
+	$gui_window::r_plot::imgs->{$self->win_name}->destroy;
+	$gui_window::r_plot::imgs->{$self->win_name} = undef;
+
+	$gui_window::r_plot::imgs->{$self->win_name} = 
+		$self->{win_obj}->Photo('photo_'.$self->win_name,
+			-file => $self->{plots}[$self->{ax}]->path,
+		)
+	;
+
+	$self->renew_command;
+}
+
 sub win_title{
 	return kh_msg->get('win_title');
 }
