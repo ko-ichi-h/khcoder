6e7ef5e0fe20a31ab440b84e3a59adcaeb0e2a0e
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2014-03-08 03:59:48 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@1769 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/plotR/network.pm
+++ core/kh_lib/plotR/network.pm
@@ -488,8 +488,9 @@ if (com_method == "com-b" || com_method == "com-g" || com_method == "com-r"){
 		com_m <- NULL
 
 		# コミュニティ数を12以下に
-		#if (F){
-		if (length( table(com$membership)[table(com$membership) > 1] ) > 12 ){
+		# →この機能は現在停止中。R（igraph）のバージョンアップ時に復活？
+		if (F){
+		#if (length( table(com$membership)[table(com$membership) > 1] ) > 12 ){
 			best_step <- 0
 			for ( i in 1:( trunc( length( com$merges ) / 2 ) - 10 ) ){
 				temp_com <- community.to.membership(n2, com$merges, i)
