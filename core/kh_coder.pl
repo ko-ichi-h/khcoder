1216feea339c936a32333efc82f15ba60f3348fa
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2014-04-11 15:53:57 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@1782 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_coder.pl
+++ core/kh_coder.pl
@@ -127,6 +127,10 @@ use kh_projects;
 use kh_morpho;
 use gui_window;
 
+# Say hello
+print "This is KH Coder $kh_version on $^O.\n";
+print "CWD: ", $config_obj->cwd, "\n";
+
 # Windows版パッケージ用の初期化
 if (
 	   ($::config_obj->os eq 'win32')
