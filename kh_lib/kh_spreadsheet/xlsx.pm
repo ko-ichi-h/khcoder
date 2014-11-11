e1ba28eaaf3b2535b476ea18e3dec2d8dbbb1c88
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2014-11-11 16:46:11 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@1855 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- kh_lib/kh_spreadsheet/xlsx.pm
+++ kh_lib/kh_spreadsheet/xlsx.pm
@@ -4,7 +4,54 @@ use strict;
 use warnings;
 use base 'kh_spreadsheet';
 
+use Spreadsheet::ParseXLSX;
 
+no warnings 'redefine';
+*Spreadsheet::ParseXLSX::_parse_shared_strings = \&_parse_shared_strings;
+use warnings 'redefine';
 
+sub _parse_shared_strings {
+	my $self = shift;
+	my ($strings) = @_;
+
+	return [
+		map {
+			my $node = $_;
+			# XXX this discards information about formatting within cells
+			# not sure how to represent that
+			#{ Text => join('', map { $_->text } $node->find_nodes('.//t')) }
+			{ Text => &remove_rph_text($node) }
+		} $strings->find_nodes('//si')
+	];
+}
+
+sub remove_rph_text {
+	my $node = shift;
+
+	# no Yomi-Gana
+	my @rph = $node->find_nodes('.//rPh');
+	unless (@rph){
+		return join('', map { $_->text } $node->find_nodes('.//t'));
+	}
+
+	# exclude Yomi-Gana
+	my $t = '';
+	my @t = $node->find_nodes('.//t');
+
+	my %check = ();                     # Yomi-Gana
+	foreach my $h (@rph){
+		$check{$h->text} = 1;
+	}
+	
+	foreach my $i (@t){                 # Text
+		my $c = $i->text;
+		$t .= $c unless $check{$c};
+	}
+	return $t;
+}
+
+sub parser{
+	return Spreadsheet::ParseXLSX->new;
+}
 
 1;
