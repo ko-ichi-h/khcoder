6fb5cb92a5d64ea04f38c93f742f83a658e2f493
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2015-07-11 16:29:49 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk/core@1959 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- utils/publisher.pl
+++ utils/publisher.pl
@@ -12,9 +12,9 @@ use strict;
 $Archive::Tar::DO_NOT_USE_PREFIX = 1;
 
 # 初期設定
-my $V = '2b32g';
+my $V = '2b32h';
 my $V_main = "2.Beta.32";
-my $V_full = "2.Beta.32g";
+my $V_full = "2.Beta.32h";
 
 # マニュアル・チュートリアルのPDFを再作成するか
 my $pdf = 0;
