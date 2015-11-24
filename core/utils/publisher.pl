65484ce6e1af4cc1712a932efb5cbe7b9816c032
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2015-11-24 12:50:12 +0000

    no message
    git-svn-id: https://svn.code.sf.net/p/khc/svn/branches/unicode@2051 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/utils/publisher.pl
+++ core/utils/publisher.pl
@@ -12,9 +12,9 @@ use strict;
 $Archive::Tar::DO_NOT_USE_PREFIX = 1;
 
 # 初期設定
-my $V = '3a04';
+my $V = '3a04a';
 my $V_main = "3.Alpha.04";
-my $V_full = "3.Alpha.04";
+my $V_full = "3.Alpha.04a";
 
 # マニュアル・チュートリアルのPDFを再作成するか
 my $pdf = 0;
