09cd854a4d1d6e7c1667686e9e84419bfc3dfa8a
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2016-01-31 12:07:08 +0000

    copy on MAC
    git-svn-id: https://svn.code.sf.net/p/khc/svn/branches/unicode@2118 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/kh_lib/kh_clipboard.pm
+++ core/kh_lib/kh_clipboard.pm
@@ -3,20 +3,24 @@ package kh_clipboard;
 use strict;
 use utf8;
 
+# Usage:
+# kh_clipboard->string( $text_data );
+
 sub string{
 	my $class = shift;
 	my $string = shift;
 
-	$::main_gui->win_obj->clipboardClear;
-	$::main_gui->win_obj->clipboardAppend($string);
+	if ( $::config_obj->os eq 'win32' ){
+		$::main_gui->win_obj->clipboardClear;
+		$::main_gui->win_obj->clipboardAppend($string);
+	} else {
+		require Clipboard;
+		Clipboard->import() unless $Clipboard::driver;
+		Clipboard->copy( Encode::encode($::config_obj->os_code,$string) );
+	}
 
 	return 1;
 }
 
 
 1;
-
-
-# この方法では日本語Windowsで中国語（Unicode文字）を扱えない
-# use Clipboard;
-# Clipboard->copy( Encode::encode($::config_obj->os_code,$clip) );
