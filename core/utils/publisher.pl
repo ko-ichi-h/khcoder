60ef42e1dfd090041ecb7a2b6047e0a6c4f5a318
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2018-03-19 16:41:43 +0000

    git-svn-id: https://svn.code.sf.net/p/khc/svn/trunk@2418 ab1598f5-0ef3-491f-8894-8c1f2390833e

--- core/utils/publisher.pl
+++ core/utils/publisher.pl
@@ -94,10 +94,10 @@ use File::Path 'rmtree';
 #                                     é¿çs
 #------------------------------------------------------------------------------
 
-&web;
+#&web;
 	#&pdfs if $pdf;
-&source_tgz;
-&win_pkg;
+#&source_tgz;
+#&win_pkg;
 	#&win_upd;
 	#&win_strb;
 &upload;
