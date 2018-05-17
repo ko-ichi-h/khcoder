d2c07a24457715fa60805209c62aed655775b6e9
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2018-05-18 02:20:26 +0900

    Getting ready for GitHub...

--- utils/publisher.pl
+++ utils/publisher.pl
@@ -129,9 +129,12 @@ sub upload{
 	close $fh;
 	$ENV{GITHUB_TOKEN} = $token;
 	
+	#system("github-release release --user ko-ichi-h --repo khc --tag $V_full --name $V_full --description \"$V\" ");
+	
 	print "Uploading...\n";
 	system("github-release --verbose upload --user ko-ichi-h --repo khc --tag $V_full --name \"khcoder-$V.exe\" --file khcoder-$V.exe ");
 	
+	# I got the 504 error from the above line.
 	
 	
 	
