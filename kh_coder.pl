d0bbc44528c2788fd2e82a80fd57dbb3f6014cdf
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2018-05-24 01:11:28 +0900

    version number

--- kh_coder.pl
+++ kh_coder.pl
@@ -20,7 +20,7 @@ use strict;
 
 use vars qw($config_obj $project_obj $main_gui $splash $kh_version);
 
-$kh_version = "3.Alpha.13d";
+$kh_version = "3.Alpha.13e";
 
 BEGIN {
 	# デバッグ用…
--- utils/publisher.pl
+++ utils/publisher.pl
@@ -14,8 +14,8 @@ $| = 1;
 $Archive::Tar::DO_NOT_USE_PREFIX = 1;
 
 # 初期設定
-my $V = '3a13d';
-my $V_full = "3.Alpha.13d";
+my $V = '3a13e';
+my $V_full = "3.Alpha.13e";
 
 # マニュアル・チュートリアルのPDFを再作成するか
 my $pdf = 0;
