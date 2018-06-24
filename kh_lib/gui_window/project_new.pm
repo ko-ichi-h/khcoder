60cfcd806446b6b1d994f1342d7074af8c0e963a
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2018-06-24 19:28:09 +0900

    fixed RTF to TXT conversion

--- kh_lib/gui_window/project_new.pm
+++ kh_lib/gui_window/project_new.pm
@@ -326,7 +326,7 @@ sub _make_new{
 			}
 			my $file_text = $1."_txt$n.txt";
 			# convert using textutil (macOS's built in tool)
-			system("textutil $t -convert txt -output $file_text");
+			system("textutil \"$t\" -convert txt -output \"$file_text\"");
 			$t = $file_text;
 			print "### The target file was NOT in TEXT format. So I converted it! ###\n";
 		}
