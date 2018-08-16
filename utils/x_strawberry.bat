fd9a73c7edb02815cb7180e4e7037694cb900598
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2018-08-17 04:59:46 +0900

    Getting ready to use Strawberry Perl on Windows

---
+++ utils/x_strawberry.bat
@@ -0,0 +1,20 @@
+@echo off
+cd /d %~dp0
+
+if "%PROCESSOR_ARCHITECTURE%" EQU "AMD64" (
+  goto :ARCH_X64
+) else (
+  goto :ARCH_X86
+)
+
+:ARCH_X86
+call C:\apps\strawberry-perl-5.22.3.1-32bit-portable\portableshell.bat /SETENV
+goto :KHCODER
+
+:ARCH_X64
+call C:\apps\strawberry-perl-5.22.3.1-64bit-portable\portableshell.bat /SETENV
+goto :KHCODER
+
+:KHCODER
+perl -MConfig -e "printf("""Perl executable: %%s\nPerl version   : %%vd $Config{archname}\n\n""", $^X, $^V)" 2>nul
+perl kh_coder.pl
