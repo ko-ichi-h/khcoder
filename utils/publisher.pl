c12e5c6cbfec273575f2e30c6562c6ef0d9a3680
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2018-12-14 21:29:47 +0900

    changed version number

--- utils/publisher.pl
+++ utils/publisher.pl
@@ -14,8 +14,8 @@ $| = 1;
 $Archive::Tar::DO_NOT_USE_PREFIX = 1;
 
 # 初期設定
-my $V = '3a15b';
-my $V_full = "3.Alpha.15b";
+my $V = '3a15c';
+my $V_full = "3.Alpha.15c";
 
 # マニュアル・チュートリアルのPDFを再作成するか
 my $pdf = 0;
