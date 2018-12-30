f006acafe0285cd21600d4e9feb26d07405731f4
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2018-12-30 16:08:14 +0900

    changed version number to 3.Alpha.15e

--- utils/publisher.pl
+++ utils/publisher.pl
@@ -14,8 +14,8 @@ $| = 1;
 $Archive::Tar::DO_NOT_USE_PREFIX = 1;
 
 # 初期設定
-my $V = '3a15d';
-my $V_full = "3.Alpha.15d";
+my $V = '3a15e';
+my $V_full = "3.Alpha.15e";
 
 # マニュアル・チュートリアルのPDFを再作成するか
 my $pdf = 0;
