57558b3584b824e862ea729c20bb9929d03ec11b
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2019-01-24 22:50:52 +0900

    trying out Tk::Dialog for issue #33

--- kh_lib/gui_errormsg/print/gui_tk.pm
+++ kh_lib/gui_errormsg/print/gui_tk.pm
@@ -17,22 +17,24 @@ sub print{
 	}
 	
 	if ($window){
-		#$window->messageBox(
-		#	-icon => $self->icon,
-		#	-type => 'OK',
-		#	-title => 'KH Coder',
-		#	-message => gui_window->gui_jchar("$self->{msg}"),
-		#);
-		
-		require Tk::Dialog;
-		my $dialog_win = $window->Dialog(
-			-title => 'KH Coder',
-			-text => gui_window->gui_jchar("$self->{msg}"),
-			-bitmap => $self->icon,
-			-default_button => 'OK',
-			-buttons => [kh_msg->gget('ok')],
-		);
-		$dialog_win->Show;
+		if ($^O eq 'darwin') {
+			require Tk::Dialog;
+			my $dialog_win = $window->Dialog(
+				-title => 'KH Coder',
+				-text => gui_window->gui_jchar("$self->{msg}"),
+				-bitmap => $self->icon,
+				-default_button => 'OK',
+				-buttons => [kh_msg->gget('ok')],
+			);
+			$dialog_win->Show;
+		} else {
+			$window->messageBox(
+				-icon => $self->icon,
+				-type => 'OK',
+				-title => 'KH Coder',
+				-message => gui_window->gui_jchar("$self->{msg}"),
+			);
+		}
 
 	} else {
 		# use Win32;
