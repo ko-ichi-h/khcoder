2bf2b3b6efef717ef0f4d47b698270386b2c30bb
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2019-02-07 22:44:46 +0900

    minor: changed 2018 to 2019

--- kh_lib/gui_window/about.pm
+++ kh_lib/gui_window/about.pm
@@ -183,7 +183,7 @@ sub _new{
 	}
 
 	$fra_r->Label(
-		-text => $self->gui_jchar($copy_mark.'2001-2018'),
+		-text => $self->gui_jchar($copy_mark.'2001-2019'),
 		-font => "TKFN",
 	)->pack(-anchor => 'nw', -pady=>'2', -side => 'left');
 
