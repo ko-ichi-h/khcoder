fa64c3fdc0358a9c78d975c20da33b183c3189f4
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2019-07-11 21:31:07 +0900

    Changed version number to 3.Alpha.16k

--- utils/publisher.pl
+++ utils/publisher.pl
@@ -14,8 +14,8 @@ $| = 1;
 $Archive::Tar::DO_NOT_USE_PREFIX = 1;
 
 # 初期設定
-my $V = '3a16j';
-my $V_full = "3.Alpha.16j";
+my $V = '3a16k';
+my $V_full = "3.Alpha.16k";
 
 # マニュアル・チュートリアルのPDFを再作成するか
 my $pdf = 0;
