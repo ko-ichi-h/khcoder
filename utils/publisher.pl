8ded831b855f78ce5bf2d9143c537e6059320113
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2019-08-30 16:07:52 +0900

    changed the version number to 3.Alpha.17d

--- utils/publisher.pl
+++ utils/publisher.pl
@@ -14,8 +14,8 @@ $| = 1;
 $Archive::Tar::DO_NOT_USE_PREFIX = 1;
 
 # 初期設定
-my $V = '3a17c';
-my $V_full = "3.Alpha.17c";
+my $V = '3a17d';
+my $V_full = "3.Alpha.17d";
 
 # マニュアル・チュートリアルのPDFを再作成するか
 my $pdf = 0;
