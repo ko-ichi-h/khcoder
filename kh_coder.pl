eb26108ade92f6908cf5808e61d123dce2eb4f99
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2019-09-17 02:31:19 +0900

    Changed the version number to 3.Alpha.17g

--- kh_coder.pl
+++ kh_coder.pl
@@ -20,7 +20,7 @@ use strict;
 
 use vars qw($config_obj $project_obj $main_gui $splash $kh_version);
 
-$kh_version = "3.Alpha.17f";
+$kh_version = "3.Alpha.17g";
 
 BEGIN {
 	# open (STDERR,">stderr.txt") or die; # for debug
