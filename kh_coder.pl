5f6f51ec1304a61b88ce1c9670477e5ec3e8b35f
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2019-11-17 00:10:41 +0900

    changed the version number to 3.Alpha.17j

--- kh_coder.pl
+++ kh_coder.pl
@@ -20,7 +20,7 @@ use strict;
 
 use vars qw($config_obj $project_obj $main_gui $splash $kh_version);
 
-$kh_version = "3.Alpha.17i";
+$kh_version = "3.Alpha.17j";
 
 BEGIN {
 	# open (STDERR,">stderr.txt") or die; # for debug
--- utils/publisher.pl
+++ utils/publisher.pl
@@ -14,8 +14,8 @@ $| = 1;
 $Archive::Tar::DO_NOT_USE_PREFIX = 1;
 
 # 初期設定
-my $V = '3a17i';
-my $V_full = "3.Alpha.17i";
+my $V = '3a17j';
+my $V_full = "3.Alpha.17j";
 
 # マニュアル・チュートリアルのPDFを再作成するか
 my $pdf = 0;
