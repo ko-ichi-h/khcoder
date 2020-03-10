ko-ichi-h/khcoder

commit f39807e7348f8c86aca701a35eafe2043848322c

Fix for #128: print human readable error messages

kh_lib/kh_cod.pm
@@ -77,8 +77,14 @@ sub read_file{
 		#$_ = Jcode->new("$_",$icode)->euc;
 		if ( $_ =~ /^＊.+/o || $_ =~ /^\*.+/o ){
 			$head = $_;
+			if ( defined($codes{$head}) ){
+				$codes{$head} = '';
+				gui_errormsg->open(
+					type => 'msg',
+					msg  => $head.kh_msg->get('redefined')
+				);
+			}
 			push @codes, $head;
-			#print Jcode->new("head: $head\n")->sjis;
 		} else {
 			$codes{$head} .= "$_\n";
 			#print Jcode->new("condition: $_\n")->sjis;

kh_lib/kh_cod/a_code.pm
@@ -111,13 +111,13 @@ sub code{
 			$words .= $i;
 			++$n;
 		}
-		$words = '-1' if $error_flag;
+		#$words = '-1' if $error_flag;
 		$self->cache_regist(
 			tani   => $tani,
 			kind   => $kind,
 			name   => $raw,
 			hyosos => $words,
-		);
+		) unless $error_flag; # do not regist cache if there is a syntax error
 	}
 	
 	# キャッシュを$self->{res_table}にコピー

config/msg.en
@@ -1077,6 +1077,7 @@ gui_errormsg::file:
     could_not_open_the_file:  "Could not open the file.\nKH Coder will exit now.\n* "
 kh_cod:
     not_coding_rules:         'The selected file does not look like a coding rule file'
+    redefined:                " code is redefined.\nPlease change the name of the code."
 kh_r_plot:
     illegal_plot_size:        'Illegal plot size'
     faliled_in_plotting:      "Failed in plotting with R\n\n"

config/msg.jp
@@ -1081,6 +1081,7 @@ gui_errormsg::file:
     could_not_open_the_file:  "ファイルを開けませんでした。\nKH Coderを終了します。\n* "
 kh_cod:
     not_coding_rules:         '選択されたファイルはコーディング・ルール・ファイルに見えません。'
+    redefined:                " コードが何度も定義されています。\nコードの名前を変更してください。"
 kh_r_plot:
     illegal_plot_size:        'プロットサイズの指定が不正です'
     faliled_in_plotting:      "Rを用いた推定または描画に失敗しました\n\n"

commit 6e46fd145833ff87977cd03d76daf103c857811d

Fixed a bug introduced in f39807e7348f8c86aca701a35eafe2043848322c: 0 frequency code raises MySQL error

kh_lib/kh_cod.pm
@@ -83,6 +83,7 @@ sub read_file{
 					type => 'msg',
 					msg  => $head.kh_msg->get('redefined')
 				);
+				return 0;
 			}
 			push @codes, $head;
 		} else {

kh_lib/kh_cod/a_code.pm
@@ -5,7 +5,7 @@ use mysql_exec;
 use strict;
 use utf8;
 
-my $debug = 0;
+my $debug = 1;
 
 #----------------------#
 #   コーディング実行   #
@@ -111,13 +111,13 @@ sub code{
 			$words .= $i;
 			++$n;
 		}
-		#$words = '-1' if $error_flag;
+		$words = '-1' if $error_flag;
 		$self->cache_regist(
 			tani   => $tani,
 			kind   => $kind,
 			name   => $raw,
 			hyosos => $words,
-		) unless $error_flag; # do not regist cache if there is a syntax error
+		); # unless $error_flag; # do not regist cache if there is a syntax error
 	}
 	
 	# キャッシュを$self->{res_table}にコピー