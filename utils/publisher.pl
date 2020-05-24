61dc61374585c546625058aa2c3e52eab4be77a1
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2020-05-25 01:20:25 +0900

    changed the version number to 3.Beta.01b

--- utils/publisher.pl
+++ utils/publisher.pl
@@ -14,8 +14,8 @@ $| = 1;
 $Archive::Tar::DO_NOT_USE_PREFIX = 1;
 
 # 初期設定
-my $V = '3b01a';
-my $V_full = "3.Beta.01a";
+my $V = '3b01b';
+my $V_full = "3.Beta.01b";
 
 # マニュアル・チュートリアルのPDFを再作成するか
 my $pdf = 0;
