712be9a40c03d9dac6f5892e1e2a364801dc2300
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2021-03-17 03:55:19 +0900

    functionality for "Find Optimal N of Topics" command

---
+++ kh_lib/gui_window/r_plot/topic_n_perplexity.pm
@@ -0,0 +1,24 @@
+package gui_window::r_plot::topic_n_perplexity;
+use base qw(gui_window::r_plot);
+
+sub option1_options{
+	return [ 'nothing' ];
+}
+
+sub option1_name{
+	return '';
+}
+
+sub win_title{
+	return kh_msg->get('win_title');
+}
+
+sub win_name{
+	return 'w_topic_n_perplexity_plot';
+}
+
+sub base_name{
+	return 'topic_n_perplexity';
+}
+
+1;
