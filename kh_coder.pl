140e27bbb019b9ddf0c6f1431c51d5b43f91f892
HIGUCHI Koichi <ko-ichi@ss.ritsumei.ac.jp>
2021-08-11 17:22:18 +0900

    Changed the version number to 3.Beta.03f

--- kh_coder.pl
+++ kh_coder.pl
@@ -20,7 +20,7 @@ use strict;
 
 use vars qw($config_obj $project_obj $main_gui $splash $kh_version);
 
-$kh_version = "3.Beta.03e";
+$kh_version = "3.Beta.03f";
 
 BEGIN {
 	# open (STDERR,">stderr.txt") or die; # for debug
